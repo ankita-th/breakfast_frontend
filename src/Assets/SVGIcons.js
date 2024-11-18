a03d6dd5db5daf1afc290d70a7a296223db05dec
ravindra <ravindra>
2024-11-18 11:03:29 +0530

    updated the home page design

---
+++ src/Assets/SVGIcons.js
@@ -0,0 +1,51 @@
+export const SwedenFlagIcon = (
+  <svg
+    width="18"
+    height="14"
+    viewBox="0 0 18 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <mask
+      id="mask0_80_19898"
+      maskUnits="userSpaceOnUse"
+      x="0"
+      y="0"
+      width="18"
+      height="14"
+    >
+      <rect width="18" height="14" fill="white" />
+    </mask>
+    <g mask="url(#mask0_80_19898)">
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M0 0V14H18V0H0Z"
+        fill="#3D58DB"
+      />
+      <mask
+        id="mask1_80_19898"
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="0"
+        width="18"
+        height="14"
+      >
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M0 0V14H18V0H0Z"
+          fill="white"
+        />
+      </mask>
+      <g mask="url(#mask1_80_19898)">
+        <path
+          fill-rule="evenodd"
+          clip-rule="evenodd"
+          d="M5.625 0H7.875V5.83333H18V8.16667H7.875V14H5.625V8.16667H0V5.83333H5.625V0Z"
+          fill="#FECA00"
+        />
+      </g>
+    </g>
+  </svg>
+);
