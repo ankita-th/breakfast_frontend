a99b9144b96360772fe0fe1aea30ef9a97184b27
Damini <damini@avioxtechnologies.com>
2024-11-19 10:02:24 +0530

    Worked on premium card

---
+++ src/app/_constant/Constant.js
@@ -0,0 +1,118 @@
+import serviceCardImg from "../../Assets/Images/service card1.svg";
+import serviceCardImg1 from "../../Assets/Images/service card2.svg";
+import serviceCardImg2 from "../../Assets/Images/service card3.svg";
+import breadImg from "../../Assets/Images/breadimg.jpg";
+
+export const SERVICE_CARD = [
+  {
+    logo_img: serviceCardImg,
+    heading: "Fresh from ",
+    subHeading: "Nutrition",
+    description:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit,sed diam nonummy",
+  },
+  {
+    logo_img: serviceCardImg1,
+    heading: "Premium ",
+    subHeading: "Quality",
+    description:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit,sed diam nonummy",
+  },
+  {
+    logo_img: serviceCardImg2,
+    heading: "100% ",
+    subHeading: "Natural Product",
+    description:
+      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit,sed diam nonummy",
+  },
+];
+
+export const PREMIUM_CARD = [
+  {
+    logo_img: breadImg,
+    title: "Brown Bread",
+    old_price: "$70.00",
+    new_price: "$60.00",
+  },
+  {
+    logo_img: breadImg,
+    title: "Eggs",
+    old_price: "$80.00",
+    new_price: "$45.00",
+  },
+  {
+    logo_img: breadImg,
+    title: "White Bread",
+    old_price: "$70.00",
+    new_price: "$50.00",
+  },
+  {
+    logo_img: breadImg,
+    title: "Chocolate Muffins",
+    old_price: "$30.00",
+    new_price: "$20.00",
+  },
+];
+
+export const PRODUCT_DATA = [
+    {
+      logo_img: breadImg,
+      title: "Brown Bread",
+      old_price: "$70.00",
+      new_price: "$60.00",
+    },
+    {
+      logo_img: breadImg,
+      title: "Eggs",
+      old_price: "$80.00",
+      new_price: "$45.00",
+    },
+    {
+      logo_img: breadImg,
+      title: "White Bread",
+      old_price: "$70.00",
+      new_price: "$50.00",
+    },
+    {
+      logo_img: breadImg,
+      title: "Chocolate Muffins",
+      old_price: "$30.00",
+      new_price: "$20.00",
+    },
+    {
+        logo_img: breadImg,
+        title: "Eggs",
+        old_price: "$80.00",
+        new_price: "$45.00",
+      },
+      {
+        logo_img: breadImg,
+        title: "White Bread",
+        old_price: "$70.00",
+        new_price: "$50.00",
+      },
+      {
+        logo_img: breadImg,
+        title: "Chocolate Muffins",
+        old_price: "$30.00",
+        new_price: "$20.00",
+      },
+      {
+        logo_img: breadImg,
+        title: "Eggs",
+        old_price: "$80.00",
+        new_price: "$45.00",
+      },
+      {
+        logo_img: breadImg,
+        title: "White Bread",
+        old_price: "$70.00",
+        new_price: "$50.00",
+      },
+      {
+        logo_img: breadImg,
+        title: "Chocolate Muffins",
+        old_price: "$30.00",
+        new_price: "$20.00",
+      },
+  ];
